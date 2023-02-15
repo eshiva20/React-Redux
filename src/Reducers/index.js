b6c0cc9a8f1bd5dba8648749954be4809098221e
shiva <shiva@nimapinfotech.com>
2023-02-15 18:07:44 +0530

    initial commit

---
+++ src/Reducers/index.js
@@ -0,0 +1,8 @@
+import changeTheNumber from "./ChangeTheNumber";
+import { combineReducers } from "redux";
+
+const rootReducer=combineReducers({
+    changeTheNumber,
+})
+
+export default rootReducer;
